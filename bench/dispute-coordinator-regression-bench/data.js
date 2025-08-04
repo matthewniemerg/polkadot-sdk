afd16d56ac9a512731a666068720108953be983a
github-action-benchmark <github@users.noreply.github.com>
2025-08-04 20:57:51 +0000

    add dispute-coordinator-regression-bench (customSmallerIsBetter) benchmark result for 59fb2e7482d471a7ec4e8d3b30499497efa7b34c

--- bench/dispute-coordinator-regression-bench/data.js
+++ bench/dispute-coordinator-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754336109794,
+  "lastUpdate": 1754341071260,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "dispute-coordinator-regression-bench": [
@@ -4360,6 +4360,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "dispute-coordinator",
             "value": 0.0025249205200000005,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "22696121+sekisamu@users.noreply.github.com",
+            "name": "sekiseki",
+            "username": "sekisamu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "59fb2e7482d471a7ec4e8d3b30499497efa7b34c",
+          "message": "Fixes dust balance handling for pallet revive (#9357)\n\nfix issue: https://github.com/paritytech/contract-issues/issues/141\n\nCorrects the condition for minting a new currency unit when transferring\ndust. The condition was incorrectly checking\n`to_info.dust.saturating_add(dust) >= plank` which could lead to\nunexpected minting behavior. It now correctly checks if `to_info.dust >=\nplank` before minting.",
+          "timestamp": "2025-08-04T19:36:58Z",
+          "tree_id": "1f82f1472637c5b251995d04a7f494c144c5daf4",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/59fb2e7482d471a7ec4e8d3b30499497efa7b34c"
+        },
+        "date": 1754341053973,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 227.09999999999997,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 23.800000000000004,
+            "unit": "KiB"
+          },
+          {
+            "name": "dispute-distribution",
+            "value": 0.008665357929999982,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.0050287778799999894,
+            "unit": "seconds"
+          },
+          {
+            "name": "dispute-coordinator",
+            "value": 0.0026385609900000002,
             "unit": "seconds"
           }
         ]
